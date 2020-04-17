b76712f0d4af346b109eff4a587857b94a8a81cf
Vasyl-007 <vasyl.manyo@gmail.com>
2020-04-17 23:42:32 +0300

    pre final task manager

--- ui-todo-project/App.js
+++ ui-todo-project/App.js
@@ -8,19 +8,19 @@ import { MainScreen } from "./src/screens/MainScreen";
 import { TodoScreen } from "./src/screens/TodoScreen";
 
 export default function App() {
-  const [todoId, setTodoId] = useState("id-10");
+  const [todoId, setTodoId] = useState(null);
 
   const [todos, setTodos] = useState([
-    { id: "id-10", title: "Task 10" },
-    { id: "id-9", title: "Task 9" },
-    { id: "id-8", title: "Task 8" },
-    { id: "id-7", title: "Task 7" },
-    { id: "id-6", title: "Task 6" },
-    { id: "id-5", title: "Task 5" },
-    { id: "id-4", title: "Task 4" },
-    { id: "id-3", title: "Task 3" },
-    { id: "id-2", title: "Task 2" },
-    { id: "id-1", title: "Task 1" },
+    // { id: "id-10", title: "Task 10" },
+    // { id: "id-9", title: "Task 9" },
+    // { id: "id-8", title: "Task 8" },
+    // { id: "id-7", title: "Task 7" },
+    // { id: "id-6", title: "Task 6" },
+    // { id: "id-5", title: "Task 5" },
+    // { id: "id-4", title: "Task 4" },
+    // { id: "id-3", title: "Task 3" },
+    // { id: "id-2", title: "Task 2" },
+    // { id: "id-1", title: "Task 1" },
   ]);
 
   const addTodo = (title) => {
@@ -65,7 +65,7 @@ export default function App() {
       })
     );
   };
-  
+
   let content = (
     <MainScreen
       todos={todos}
--- ui-todo-project/src/screens/MainScreen.js
+++ ui-todo-project/src/screens/MainScreen.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Image,
+  ImageBackground,
+} from "react-native";
 import { AddTodo } from "../components/AddTodo";
 import { Todo } from "../components/Todo";
 
 export const MainScreen = ({ todos, addTodo, removeTodo, openTodo }) => {
-  return (
+  let content = (
     <View>
-      <AddTodo onSubmit={addTodo} />
-
       <FlatList
         keyExtractor={(item) => item.id}
         data={todos}
@@ -15,8 +19,54 @@ export const MainScreen = ({ todos, addTodo, removeTodo, openTodo }) => {
           <Todo todo={item} onRemoveTodo={removeTodo} onOpen={openTodo} />
         )}
       />
+      <View>
+        <ImageBackground
+          source={require("../../assets/task-management-bg.png")}
+          style={styles.backImage}
+        ></ImageBackground>
+      </View>
+    </View>
+  );
+
+  if (todos.length === 0) {
+    content = (
+      <View style={styles.imgWrap}>
+        <Image
+          source={require("../../assets/no-results.png")}
+          style={styles.img}
+        />
+        {/* <Image
+          source={{
+            uri:
+              "https://cdn.dribbble.com/users/158194/screenshots/6398178/empty_state-01_2x.jpg",
+          }}
+          style={styles.img}
+        /> */}
+      </View>
+    );
+  }
+  return (
+    <View>
+      <AddTodo onSubmit={addTodo} />
+
+      {content}
     </View>
   );
 };
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imgWrap: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+  },
+  img: {
+    width: 380,
+    height: 300,
+    resizeMode: "cover",
+  },
+  backImage: {
+    height: 250,
+    resizeMode: "contain",
+  },
+});
